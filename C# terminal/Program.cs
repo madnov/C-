144476100ca9007e687d482976b3bc2b105da6a7
Max Navasyolau <madnovos@mail.ru>
2023-01-24 22:17:18 +0300

    End

--- C# terminal/Program.cs
+++ C# terminal/Program.cs
@@ -1,16 +1,45 @@
-﻿ int a = 1;
- int b = 2;
- int c = 6;
- int d = 8;
- int e = 4;
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
 
- int max = a;
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
 
- if (a > max) max = a;
- if (b > max) max = b;
- if (c > max) max = c;
- if (d > max) max = d;
- if (e > max) max = e;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
 
- Console.Write("max = ");
- Console.WriteLine(max);
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while(count < 10000)
+{
+    int what = new Random().Next(0, 3); 
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
