6070d3557309294a02a6a61562adb0199cb23d85
Max Navasyolau <madnovos@mail.ru>
2023-01-26 18:41:47 +0300

    add task1

--- task_2.cs
+++ task_2.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите первое число: ");
+Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
