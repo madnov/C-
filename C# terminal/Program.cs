56e7e4a71b9eeeadc7284cc38800a71c3e846bb0
Max Navasyolau <madnovos@mail.ru>
2023-01-27 19:37:16 +0300

    Friday

--- C# terminal/Program.cs
+++ C# terminal/Program.cs
@@ -1,10 +1,1 @@
-﻿// Задача 8
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-for(int i = 1; i <= number; i++)
-{
-    if(i % 2 == 0)
-    {
-        Console.WriteLine(i);
-    }
-}
+﻿
