681fc37a7904e9a3f39ab443c4f7d85b1c9f960b
Max Navasyolau <madnovos@mail.ru>
2023-01-28 18:06:35 +0300

    Task_10 Changes

--- HomeWork_2/Task_10.cs
+++ HomeWork_2/Task_10.cs
@@ -2,6 +2,12 @@
 Console.Clear();
 Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+
+while(number < 100 || number > 999 )
+{
+    Console.Write("Ошибка!\nВведите трёхзначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
 int result = (number / 10) % 10;
 
 Console.WriteLine(result);
