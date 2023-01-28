aea9051c028ecd47c091740536f3cc45390bd55f
Max Navasyolau <madnovos@mail.ru>
2023-01-28 17:41:45 +0300

    HomeWork_2 resolved Task_10

--- CSharp terminal/Program.cs
+++ CSharp terminal/Program.cs
@@ -1,17 +1,7 @@
-﻿// Задача 15
+﻿// Задача 10
 Console.Clear();
-Console.Write("Введите цифру от 1 до 7: ");
+Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-while( number < 1 || number > 7)
-{
-  Console.Write("Вы ошиблись!\nВведите цифру от 1 до 7: ");
-  number = Convert.ToInt32(Console.ReadLine());
-}
+int result = (number / 10) % 10;
 
-if(number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
-
-   Console.WriteLine("нет");
-
-else
-  Console.WriteLine("да");
-    
+Console.WriteLine(result);
--- HomeWork_2/Task_15.cs
+++ HomeWork_2/Task_15.cs
@@ -2,7 +2,7 @@
 Console.Clear();
 Console.Write("Введите цифру от 1 до 7: ");
 int number = Convert.ToInt32(Console.ReadLine());
-while ( number < 1 || number > 7)
+while (number < 1 || number > 7)
 {
     Console.Write("Вы ошиблись!\nВведите цифру от 1 до 7: ");
     number = Convert.ToInt32(Console.ReadLine());
