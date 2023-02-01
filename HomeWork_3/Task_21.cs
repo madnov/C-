a8a716c11faf75fe1a55e0e75db2a48bafba3a6a
Max Navasyolau <madnovos@mail.ru>
2023-02-01 19:57:00 +0300

    Wednesday

--- HomeWork_3/Task_21.cs
+++ HomeWork_3/Task_21.cs
@@ -14,9 +14,9 @@ int b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третью координату B: ");
 int b3 = Convert.ToInt32(Console.ReadLine());
 
- int res1 = a1 - b1;
- int res2 = a2 - b2;
- int res3 = a3 - b3;
+ int res1 = b1 - a1;
+ int res2 = b2 - a2;
+ int res3 = b3 - a3;
 
 int res4 = (res1 * res1) + (res2 * res2) + (res3 * res3);
 double result = Math.Sqrt(res4);
