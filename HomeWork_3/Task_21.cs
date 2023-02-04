0b02096485627d114280942b25ea313c74661840
Max Navasyolau <madnovos@mail.ru>
2023-02-04 13:43:58 +0300

    Resolved HomeWork_3

--- HomeWork_3/Task_21.cs
+++ HomeWork_3/Task_21.cs
@@ -16,4 +16,4 @@ Console.Write("Введите координату Z(2): ");
 double z2 = Convert.ToDouble(Console.ReadLine()) ;
 
 double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)); 
-Console.WriteLine(Math.Round(result,2));
+Console.WriteLine(Math.Round(result, 2));
