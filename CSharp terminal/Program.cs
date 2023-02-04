fac5b873990bf6ca93b0de4019e30ce446dac34c
Max Navasyolau <madnovos@mail.ru>
2023-02-04 16:57:01 +0300

    After lection

--- CSharp terminal/Program.cs
+++ CSharp terminal/Program.cs
@@ -1,18 +1,31 @@
-﻿// Задача 19
-Console.Clear();
+﻿Console.Clear();
 
-Console.Write("Введите пятизначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-while (number < 10000 || number > 99999)
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-        Console.Write("Ошибка!\nВведите пятизначное число: ");
-        number = Convert.ToInt32(Console.ReadLine());
-    }    
-int num1 = number / 10000;
-int num2 = (number / 1000) % 10;
-int num3 = number % 10;
-int num4 = (number / 10) % 10;
-if (num1 == num3 && num2 == num4)
-    Console.WriteLine("да");
-else
-    Console.WriteLine("нет");
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+SelectionSort(arr);
+PrintArray(arr);
