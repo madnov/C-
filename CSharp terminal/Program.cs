9aea8239ce64f5bc79eb9554ab357782451a558c
Max Navasyolau <madnovos@mail.ru>
2023-02-17 17:21:24 +0300

    Resolved HomeWork_7 Friday

--- CSharp terminal/Program.cs
+++ CSharp terminal/Program.cs
@@ -2,31 +2,46 @@
 
 void FillArray(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)    
+    for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
-            array[i, j] = new Random().Next(1,30);   
+            array[i, j] = new Random().Next(1, 10);
 }
 
-void FindSum(int[,] array)
+void FindAverage(int[,] array)
 {
-       
+    Console.WriteLine("Cреднее арифметическое\nстолбцов: ");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double total = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            total += array[j, i];
+        }
+        Console.Write($"{Math.Round(total / array.GetLength(0), 2)}; ");
+    }
 }
+
 void PrintArray(int[,] array)
 {
+    Console.WriteLine("Заданный двумерный\nмассив: ");
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             Console.Write($"{array[i, j]} ");
         }
-    Console.WriteLine();    
-    }        
-} 
+        Console.WriteLine();
+    }
+}
 
 Console.Clear();
-int [,] array = new int[5, 4];
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+
 FillArray(array);
 PrintArray(array);
-FindSum(array);
-
-
+Console.WriteLine();
+FindAverage(array);
